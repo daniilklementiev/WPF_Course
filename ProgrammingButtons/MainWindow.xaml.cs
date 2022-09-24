5e0c5df66d0b960a736a71a9877e1d5b38ec6de5
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-09-24 19:17:09 +0400

    +Grid Buttons

---
+++ ProgrammingButtons/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgrammingButtons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Grid grid = new Grid();
+            grid.ShowGridLines = true;
+            
+            // Создать по 1 кнопке в каждой ячейке grid
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    var row = new RowDefinition();
+                    row.Height = GridLength.Auto;
+
+                    var col = new ColumnDefinition();
+                    col.Width = GridLength.Auto;
+
+                    grid.ColumnDefinitions.Add(col);
+                    grid.RowDefinitions.Add(row);
+
+                    var button = new Button();
+                    button.Width = 30;
+                    button.Height = 30;
+                    Grid.SetColumn(button, j);
+                    Grid.SetRow(button, i);
+                    grid.Children.Add(button);
+                }
+
+            }
+
+            this.Content = grid;
+        }
+    }
+ }
